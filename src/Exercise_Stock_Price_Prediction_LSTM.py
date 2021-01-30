91273ad116206583c431909c58d192985465df83
German Mesa <german.mesa@sap.com>
2021-01-30 10:19:59 +0100

    Changes to Stock Price predicitons

--- src/Exercise_Stock_Price_Prediction_LSTM.py
+++ src/Exercise_Stock_Price_Prediction_LSTM.py
@@ -22,38 +22,29 @@ def dataset_review(data):
 
 
 def time_series_visualization(data):
-    # Plot all together
-    plt.plot(data['High'], linestyle='dotted', color='skyblue', linewidth=0.5)
     plt.plot(data['Close'], linestyle='solid', color='skyblue', linewidth=1)
-    plt.plot(data['Low'], linestyle='dotted', linewidth=0.5)
     plt.xlabel('Date')
-    plt.ylabel('Price USD')
-    plt.legend()
-    plt.show()
-
-    # Subplots by price type
-    axes = data[['High', 'Low', 'Open', 'Close', 'Volume']].plot(marker='.',
-                                                                 alpha=0.5,
-                                                                 linestyle='None',
-                                                                 figsize=(11, 9),
-                                                                 subplots=True)
-
-    for ax in axes:
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Price USD')
+    plt.ylabel('Close Price USD')
 
     plt.show()
+
+
+def build_model_stock_price_prediction(data):
+    pass
 
 
 def main():
-    # Read stock prices dataset from Yahoo
+    # Read daily stock prices dataset from Yahoo
     df = web.DataReader('MSFT', data_source='yahoo', start='2012-01')
 
     # Review dataset
     dataset_review(df)
 
-    # Visualization capabilities
+    # Time series visualization
     time_series_visualization(df)
+
+    # Built model for stock price prediction
+    model = build_model_stock_price_prediction(df)
 
 
 if __name__ == '__main__':
