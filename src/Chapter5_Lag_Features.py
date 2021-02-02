59fb6d59ec0573263b19493f39818bfb7e23d5bd
german-mesa <german.mesa@sap.com>
2021-02-02 17:24:46 +0100

    Time series forecasting with Python - Book

--- src/Chapter5_Lag_Features.py
+++ src/Chapter5_Lag_Features.py
@@ -10,17 +10,18 @@ def main():
                       parse_dates=[0])
 
     # Sliding window approach
+    # The simplest approach is to predict the value at the next time (t+1) given the value
+    # at the current time (t)
+    data = DataFrame(series.values)
+    data = concat([data.shift(1), data], axis=1)
+    data.columns = ['Shifted', 'Original']
+    print(data.head(10))
+
+    # Now we include the last 3 observed values to predict the value at the next time step
     data = DataFrame(series.values)
     data = concat([data.shift(3), data.shift(2), data.shift(1), data], axis=1)
     data.columns = ['t-2', 't-1', 't', 't+1']
-
-    # Some dataset info
-    print(data.dtypes)
-    print(data.describe())
     print(data.head(10))
-
-    print('Number of records %d' % data.size)
-    print(data.memory_usage(deep=True))
 
 
 if __name__ == '__main__':
